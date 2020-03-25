c55e33f3d3db35b390bd9184f2a8f589700b17df
mjoellnier <other@max-wuest.de>
2020-03-25 10:15:25 +0100

    Styling adjustments

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./assets/images/logo.svg";
+import "./style/App.css";
+import GitHubButton from "react-github-btn";
+import { TwitterFollowButton } from "react-twitter-embed";
 
 function App() {
   return (
@@ -8,16 +10,27 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is the test PWA for my{" "}
+          <a
+            href="https://github.com/mjoellnier/react-push-project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React Push Project
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="socialBanner">
+          <TwitterFollowButton screenName={"coding_max"} />
+          <GitHubButton
+            href="https://github.com/mjoellnier"
+            data-color-scheme="no-preference: dark; light: dark; dark: dark;"
+            data-size="large"
+            aria-label="Follow @mjoellnier on GitHub"
+            style={{ float: "right" }}
+          >
+            Follow @mjoellnier
+          </GitHubButton>
+        </div>
       </header>
     </div>
   );
